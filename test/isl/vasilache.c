77232f1dccf225591d9decc983e955c75c9537d8
Sven Verdoolaege <skimo@kotnet.org>
2009-03-08 19:25:10 +0100

    update isl for normalization during convex hull computation
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- test/isl/vasilache.c
+++ test/isl/vasilache.c
@@ -1,27 +1,27 @@
-/* Generated from ../../../git/cloog/test/vasilache.cloog by CLooG 0.14.0-136-gb91ef26 gmp bits in 0.49s. */
-S1() ;
-S2() ;
+/* Generated from ../../../git/cloog/test/vasilache.cloog by CLooG 0.14.0-155-g5d9f3a7 gmp bits in 0.38s. */
+S1();
+S2();
 for (p1=0;p1<=N-1;p1++) {
   for (p3=0;p3<=N-1;p3++) {
-    S4(p1,p3) ;
-    S5(p1,p3) ;
+    S4(p1,p3);
+    S5(p1,p3);
   }
 }
 for (p1=0;p1<=N-1;p1++) {
   for (p3=0;p3<=N-1;p3++) {
     for (p5=0;p5<=floord(N-1,32);p5++) {
-      S7(p1,p3,p5,32*p5) ;
+      S7(p1,p3,p5,32*p5);
       for (p7=32*p5+1;p7<=min(N-1,32*p5+31);p7++) {
-        S6(p1,p3,p5,p7-1) ;
-        S7(p1,p3,p5,p7) ;
+        S6(p1,p3,p5,p7-1);
+        S7(p1,p3,p5,p7);
       }
-      if (p5 >= ceild(N-31,32)) {
-        S6(p1,p3,p5,N-1) ;
+      if (p5 <= floord(N-33,32)) {
+        S6(p1,p3,p5,32*p5+31);
       }
-      if (p5 <= floord(N-32,32)) {
-        S6(p1,p3,p5,32*p5+31) ;
+      if (p5 >= max(ceild(N-32,32),ceild(27*N-768,896))) {
+        S6(p1,p3,p5,N-1);
       }
     }
   }
 }
-S8() ;
+S8();
