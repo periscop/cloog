1487fe2c5f5d06d9ffc74d3b82b167b5f7a2feeb
Sven Verdoolaege <skimo@kotnet.org>
2010-01-03 16:17:00 +0100

    remove PolyLib backend
    The PolyLib backend was tainting the whole distribution with the GPL.
    It will be made a available in a separate repository.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- include/cloog/polylib/cloog.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLOOG_POLYLIB_H
-#define CLOOG_POLYLIB_H
-
-#include <cloog/cloog.h>
-#include <cloog/matrix/constraintset.h>
-#include <cloog/polylib/backend.h>
-#include <cloog/polylib/domain.h>
-
-#endif /* define _H */
--- test/polylib/mod4.c
+++
@@ -1,8 +0,0 @@
-/* Generated from ../../../git/cloog/test/mod4.cloog by CLooG 0.14.0-250-g1187103 gmp bits in 0.01s. */
-for (j=2;j<=10;j+=3) {
-  div41 = floord(j+2,3);
-  div42 = floord(j+1,3);
-  S1(j,div41,div42,2,(j-2)/3);
-  S2(j,div41,div42,2,(j-2)/3);
-  S3(j,div41,div42,2,(j-2)/3);
-}
