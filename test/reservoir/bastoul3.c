a0b8ed180d30704a5d0ec6cc715868b6f9bef885
Sven Verdoolaege <skimo@kotnet.org>
2011-07-31 14:21:44 +0200

    update test case to reflect change in isl's isl_aff_floor
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- test/reservoir/bastoul3.c
+++ test/reservoir/bastoul3.c
@@ -1,6 +1,6 @@
-/* Generated from ../../../git/cloog/test/reservoir/bastoul3.cloog by CLooG 0.14.0-308-g2713b64 gmp bits in 0.01s. */
+/* Generated from ../../../git/cloog/test/reservoir/bastoul3.cloog by CLooG 0.16.3 gmp bits in 0.01s. */
 for (i=3;i<=9;i++) {
-  for (j=max(i-6,i-2*floord(i-1,2));j<=min(3,i-2);j+=2) {
+  for (j=max(i-6,i-2*floord(i+1,2)+2);j<=min(3,i-2);j+=2) {
     S1(i,j,(i-j)/2);
   }
 }
