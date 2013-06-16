f861f854b6cc85b67e39794e38e7104fcd59af90
Tobias Grosser <tobias@grosser.es>
2013-06-16 12:14:37 -0700

    Update isl
    isl commit f5ed5dd6 changed the output of one the test cases. We update
    isl and the test case to allow CLooG to be compiled with this and later
    versions of isl.

--- test/isl/jacobi-shared.c
+++ test/isl/jacobi-shared.c
@@ -1,10 +1,11 @@
 /* Generated from ../../../git/cloog/test/isl/jacobi-shared.cloog by CLooG 0.16.3-2-g5511bef gmp bits in 1.82s. */
 if ((h0+1)%2 == 0) {
-  if ((16*floord(N+15*g1+15*t0+15,16) >= 15*g1+15*t0+19) && (16*floord(N+15*g1+15*t0+15,16) >= 16*g1+15*t0+17) && (32*floord(t1-1,32) <= g2+t1-3) && (32*floord(t1-1,32) >= -N+g2+t1+1)) { 
+  if ((16*floord(N+15*t0+15,16) >= 15*t0+19) && (16*floord(N+15*t0+15,16) >= g1+15*t0+17) && (32*floord(t1-1,32) <= g2+t1-3) && (32*floord(t1-1,32) >= -N+g2+t1+1)) {
     for (c0=max(-16*floord(t0-1,16)+t0,-16*floord(g1+t0-3,16)+t0);c0<=min(32,N-g1-1);c0+=16) {
-      c1 = 32*floord(-t1,32)+t1+32;
-      if (c1 <= 32) {
-        S1(c0+g1-1,c1+g2-1);
+      for (c1=-32*floord(t1-1,32)+t1;c1<=min(32,N-g2-1);c1+=32) {
+        if ((c1 >= 1) && (c1 <= 32)) {
+          S1(c0+g1-1,c1+g2-1);
+        }
       }
     }
   }
