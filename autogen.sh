70fd329c076a91ccbd9455760f2b402aac916d41
CristianRCV <cristianrcv@gmail.com>
2018-04-12 11:04:40 +0200

    Make autogen.sh fail on error. Add reconfigure verbose. Do not create autoconf folder

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-autoreconf -i
+
+set -e
+
+# (Re)Generate autotools files
+autoreconf -vi
+
+# (Re)Generate autotools files for ISL if it exists
 if test -f isl/autogen.sh; then
-	(cd isl; ./autogen.sh)
+  (cd isl; ./autogen.sh)
 fi
